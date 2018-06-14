01228390533149dd652aa7b81419c78ddcf220f7
samz-winter <37353530+samz-winter@users.noreply.github.com>
2018-06-14 14:54:37 -0500

    Create search_column_names.sql

---
+++ query_storage/search_column_names.sql
@@ -0,0 +1,13 @@
+# Search Column Names
+
+Use the following query to search all column names in a schema.
+```sql
+select
+  table_schema, table_name, column_name, data_type
+from
+  information_schema.columns
+where 
+  column_name like '%cust%'
+order by 
+  1,2,ordinal_position asc
+```
